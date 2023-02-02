ef1be5c37f69d1f8d48bc42963b2940ebd98fa1c
Brett Kuskie <fullaxx@gmail.com>
2023-02-02 15:14:05 -0700

    Add alternative to scanf in hex2bin()

--- hex2bin.c
+++ hex2bin.c
@@ -4,13 +4,22 @@
 
 #include "hex2bin.h"
 
+#ifdef NOSCANF
+unsigned int toInt(char c)
+{
+	if (c >= '0' && c <= '9') { return      c - '0'; }
+	if (c >= 'A' && c <= 'F') { return 10 + c - 'A'; }
+	if (c >= 'a' && c <= 'f') { return 10 + c - 'a'; }
+	return -1;
+}
+#endif
+
 bin_pkg_t hex2bin(char *hex)
 {
 	ssize_t hlen, h=0;
 	bin_pkg_t r = { 0, 0 };
 
-	//printf("%s\n", hex);
-
+	// At the moment we are assuming only hex digits in the incoming hex array
 	hlen = strlen(hex);
 	if(hlen < 2) { return r; }
 	if(hlen % 2 != 0) { return r; }
@@ -18,11 +27,12 @@ bin_pkg_t hex2bin(char *hex)
 	r.size = 0;
 	r.data = malloc(hlen/2);
 	while(hlen > 1) {
-		//printf("%c%c ", *(hex+h), *(hex+h+1));
+#ifdef NOSCANF
+		r.data[r.size++] = 16 * toInt(hex[h]) + toInt(hex[h+1]);
+#else
 		sscanf(hex+h, "%02hhx", (r.data)+(r.size++));
-		//printf("%02x / ", *(blob+i));
-		hlen -= 2;
-		h += 2;
+#endif
+		hlen -= 2; h += 2;
 	}
 	return r;
 }
